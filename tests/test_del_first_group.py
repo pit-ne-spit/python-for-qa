dd6d1307e9c95f3082b183ef65ae1a6682853156
Petr Arapov <p.arapov@pravo.tech>
2022-03-12 00:54:12 +0400

    precondition for delete & modify group added

--- tests/test_del_first_group.py
+++ tests/test_del_first_group.py
@@ -1,2 +1,6 @@
+from models.group import Group
+
 def test_del_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="test"))
     app.group.delete_first_group()
--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,6 +1,8 @@
 from models.group import Group
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="test"))
     app.group.modify_first_group(Group(group_name="New group"))
 
 
