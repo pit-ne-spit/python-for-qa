043ccb0d7ec3d1dfa14c8c544b20fb2002b14ac3
Petr Arapov <p.arapov@pravo.tech>
2022-03-15 23:49:39 +0400

    equality for groups added

--- models/group.py
+++ models/group.py
@@ -5,3 +5,9 @@ class Group:
         self.group_header = group_header
         self.group_footer = group_footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.group_name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.group_name == other.group_name
