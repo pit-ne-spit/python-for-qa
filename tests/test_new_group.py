c2053389d1b20b4bb239b08b9cc01e02b4d8aad6
Petr Arapov <p.arapov@pravo.tech>
2022-03-16 00:30:10 +0400

    groups list equality sorted

--- tests/test_new_group.py
+++ tests/test_new_group.py
@@ -3,16 +3,22 @@ from models.group import Group
 
 def test_new_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name="First group", group_header="1 groupn", group_footer="The first added group"))
+    group = Group(group_name="First group", group_header="1 group", group_footer="The first added group")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_new_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name="", group_header="", group_footer=""))
+    group = Group(group_name="", group_header="", group_footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
