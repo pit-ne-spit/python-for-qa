2754e13a9d8f9e8976eee7674f9c124be39ade81
Petr Arapov <p.arapov@pravo.tech>
2022-04-08 00:45:24 +0400

    check_ui function added

--- conftest.py
+++ conftest.py
@@ -42,13 +42,18 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
--- tests/test_del_some_group.py
+++ tests/test_del_some_group.py
@@ -1,7 +1,7 @@
 import random
 from models.group import Group
 
-def test_del_some_group(app, db):
+def test_del_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="test"))
     old_groups = db.get_group_list()
@@ -11,3 +11,5 @@ def test_del_some_group(app, db):
     new_groups = db.get_group_list()
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
